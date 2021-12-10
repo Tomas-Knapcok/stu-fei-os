cad851905fd429748c2954534d50c79a19ffd630
Tomas Knapcok <xknapcok@stuba.sk>
2021-12-10 16:47:59 +0100

    feat: implemented mmap and munmap

--- kernel/proc.h
+++ kernel/proc.h
@@ -82,6 +82,17 @@ struct trapframe {
 
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+struct VMA{
+  uint64 addr;
+  uint64 end;
+  int prot;
+  int flags;
+  int fd;
+  int offset;
+  struct file *pf;
+  int used;
+};
+
 // Per-process state
 struct proc {
   struct spinlock lock;
@@ -105,4 +116,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  int originalsize;
+  struct VMA vma[16];
 };
